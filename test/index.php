7c3631a716e0380fb764fa0b72121810c6b7c766
Ismael Trascastro <itrascastro@gmail.com>
2016-07-06 10:39:03 +0000

    Testing remote access to ClearDB 3

--- test/index.php
+++ test/index.php
@@ -4,6 +4,8 @@ require_once '../config.php';
 
 $sth = $db->prepare("select * from information_schema.tables");
 $sth->execute();
-$result = $sth->fetchAll();
+$tables = $sth->fetchAll();
 
-var_dump($result);
+foreach($tables as $table) {
+    echo $table['TABLE_NAME'] . '<br />';
+}
